1565265f03dd71e6341000506ea0495feb887228
Jay Wang <wuj20061224@gmail.com>
2026-09-24 11:37:11 +0800

    (Lab2 Adv) Finish exact subset sub

--- lab2_advanced/lab2_adv_1.sv
+++ lab2_advanced/lab2_adv_1.sv
@@ -3,10 +3,10 @@
 Multiplexer
 */
 module mux (
-    input logic [3:0] a, // 0
-    input logic [3:0] b, // 1
-    input logic       sel,
-    input logic [3:0] out
+    input  logic [3:0] a, // 0
+    input  logic [3:0] b, // 1
+    input  logic       sel,
+    output logic [3:0] out
 );
 
     assign out = (sel) ? b : a;
@@ -17,14 +17,27 @@ endmodule
 Check subset_sum == target
 */
 module subset_sum (
-    input  logic [3:0] val_1,
+    input  logic [3:0] val_1, // 0
     input  logic [3:0] val_2,
     input  logic [3:0] val_3,
-    input  logic [3:0] val_4,
+    input  logic [3:0] val_4, // 3
     input  logic [3:0] subset_number,
     input  logic [5:0] target,
     output logic match
 );
+
+    logic [5:0] inter_sum;
+    logic [3:0] selected_val1, selected_val2, selected_val3, selected_val4;
+
+
+    mux m1(.a(4'b0), .b(val_1), .sel(subset_number[3]), .out(selected_val1));
+    mux m2(.a(4'b0), .b(val_2), .sel(subset_number[2]), .out(selected_val2));
+    mux m3(.a(4'b0), .b(val_3), .sel(subset_number[1]), .out(selected_val3));
+    mux m4(.a(4'b0), .b(val_4), .sel(subset_number[0]), .out(selected_val4));
+
+    assign inter_sum = selected_val1 + selected_val2 + selected_val3 + selected_val4;
+
+    assign match = ( inter_sum == target ) ? 1 : 0;
 
 endmodule
 
@@ -32,12 +45,14 @@ endmodule
 /*
 Output the position (decimal) of the first 1-bit in an array (like p_decoder)
 */
+/*
 module priorty_decoder(
     input  logic [15:0] arr,
     output logic [3:0]  pos  
 );
 
 endmodule
+*/
 
 /*
 Main module
@@ -56,25 +71,60 @@ module lab2_adv_1 (
 
     logic [15:0] match;
 
-    always_comb begin
-        
+    generate
+        genvar i;
         for ( i = 0 ; i < 16 ; i = i + 1 ) begin
             subset_sum my_subset_sum(
                 val_1, val_2, val_3, val_4, i, target, match[i]
             );
-        end
+        end                
+    endgenerate
 
-        switch (match) begin
-            casex (param)
-                : 
-                default: 
-            endcase 
 
+    always_ff @( posedge clk ) begin
+        if ( match != 0 ) 
+            exact <= 1;
+        else 
+            exact <= 0;
     end
 
     always_ff @( posedge clk ) begin
         if ( rst ) out <= 0;
-
+        else begin
+            if ( match[0] ) begin
+                out <= 0;
+            end else if ( match[1] ) begin
+                out <= 1;
+            end else if ( match[2] ) begin
+                out <= 2;
+            end else if ( match[3] ) begin
+                out <= 3;
+            end else if ( match[4] ) begin
+                out <= 4;
+            end else if ( match[5] ) begin
+                out <= 5;
+            end else if ( match[6] ) begin
+                out <= 6;
+            end else if ( match[7] ) begin
+                out <= 7;
+            end else if ( match[8] ) begin
+                out <= 8;
+            end else if ( match[9] ) begin
+                out <= 9;
+            end else if ( match[10] ) begin
+                out <= 10;
+            end else if ( match[11] ) begin
+                out <= 11;
+            end else if ( match[12] ) begin
+                out <= 12;
+            end else if ( match[13] ) begin
+                out <= 13;
+            end else if ( match[14] ) begin
+                out <= 14;
+            end else if ( match[15] ) begin
+                out <= 15;
+            end
+        end
     end
 
 endmodule
--- lab2_advanced/lab2_adv_1_t.sv
+++ lab2_advanced/lab2_adv_1_t.sv
@@ -20,6 +20,9 @@ module lab2_adv_1_tb;
     logic [5:0] test_target [0:7];
     int unsigned test_index;
 
+    logic [3:0] expected_result [7];
+    logic [3:0] expected_out;
+
     lab2_adv_1 dut (
         .clk(clk),
         .rst(rst),
@@ -84,6 +87,9 @@ module lab2_adv_1_tb;
         val_4 = 4'd0;
         target = 6'd0;
 
+        
+        expected_result = '{4'b0110, 4'b0000, 4'b0010, 4'b1111, 4'b0011, 4'b0101, 4'b0110};
+
         // The first post-reset input change occurs at the falling edge at
         // 35 ns. Each later set is applied at the next falling edge.
         #35ns;
@@ -98,6 +104,10 @@ module lab2_adv_1_tb;
             #1ns;
             // TODO: Implement your expected-result checks here, after the sampled rising edge.
             // TODO: Also verify that both outputs change only at rising edges.
+            if ( expected_result[test_index] != out ) begin
+                $display("== FAIL == (see below)");
+            end
+            
             if (test_index < 7)
                 @(negedge clk);
         end
@@ -105,7 +115,7 @@ module lab2_adv_1_tb;
 
     always @(posedge clk) begin
         #1ns;
-        $display("t=%0t rst=%b values={%0d,%0d,%0d,%0d} target=%0d out=%b exact=%b",
+        $display("t=%0t rst=%b values={%0d,%0d,%0d,%0d} target=%0d out=%b, exact=%b",
                  $time, rst, val_1, val_2, val_3, val_4, target, out, exact);
     end
 
@@ -113,6 +123,11 @@ module lab2_adv_1_tb;
     initial begin
         #120ns;
         $finish;
+    end
+
+    initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars(0, lab2_adv_1_tb); // Replace with your top-level module/testbench name
     end
 
 endmodule
